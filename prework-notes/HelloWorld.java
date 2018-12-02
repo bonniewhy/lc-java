80d20655eec265a2ef72172c3b92c8fca8e2915d
bonniewhy <bonnieinpixels@gmail.com>
2018-12-01 21:50:07 -0600

    Getting things set up.

---
+++ prework-notes/HelloWorld.java
@@ -0,0 +1,12 @@
+// Java Code for printing "Hello, World!"
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // write code you want to execute here
+
+        System.out.println("Hello, Java!");
+
+    }
+
+}
